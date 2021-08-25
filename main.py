534e4ff8d4f70a838167b04b3d7f325087b4dc3a
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-25 11:04:52 -0700

    Created baseline gui and empty classes

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import os
 import datetime as dt
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 
+
+class LoadScreen(FloatLayout):
+    select = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class MainScreen(BoxLayout):
+    pass
 
 
 class MainApp(App):
-    def build:
-        return Builder.load_file("frontend.kv")
+    def build(self):
+        Builder.load_file("frontend.kv")
+        return MainScreen()
+
 
 MainApp().run()
 
