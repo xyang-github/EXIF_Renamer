073d9e3fbb6262f9a19586c6aa83d4ac72ff07d2
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-25 11:51:25 -0700

    Added a filechooser that will display the file paths in the filenames label.

--- main.py
+++ main.py
@@ -6,15 +6,42 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 
 
-class LoadScreen(FloatLayout):
+class SelectDialog(FloatLayout):
+    """Dialog box to select image file from file browser"""
     select = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
 
 class MainScreen(BoxLayout):
-    pass
+    def select(self):
+        """Instantiates selection dialog box as a popup and opens it"""
+        select_dialog = SelectDialog(select=self.select_files, cancel=self.select_dismiss)
+        self.popup = Popup(title="Select File(s)",
+                           content=select_dialog,
+                           size_hint=(0.9, 0.9))
+        self.popup.open()
+
+    def select_dismiss(self):
+        """Dismisses the popup box"""
+        self.popup.dismiss()
+
+    def select_files(self, selection):
+        """Stores file path as a list of strings"""
+        image_list = selection
+        self.display_filenames(image_list)
+        self.select_dismiss()  # dismisses the popup once selection has been made
+
+    def display_filenames(self, image_list):
+        """Create a string variable of file paths to be displayed in filenames label"""
+        image_list_formatted = ""
+        for image in image_list:
+            image_list_formatted += image + "\n"
+        self.ids.filenames.text = image_list_formatted
+
+
 
 
 class MainApp(App):
