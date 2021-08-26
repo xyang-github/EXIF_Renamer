9a908e250a34364da7b4d03b52af891c8c988337
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-26 12:07:41 -0700

    Removed erroneous argument.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
